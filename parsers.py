0de0602c03fe1003ba24510a96738f02ed64e33d
argen7um <argen7um.music@yandex.ru>
2023-09-17 21:38:01 +0700

    [ADD] orgJSON convertoer cls

--- parsers.py
+++ parsers.py
@@ -1,18 +1,19 @@
 from abc import ABC, abstractmethod
 from string import Template
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from datetime import datetime
 from pprint import pprint
+from functools import reduce
 
 from requests import get, post
 from bs4 import BeautifulSoup
 
 import asyncio
+import aiohttp
 
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
     }
-
 
 
 class Parser:
@@ -30,10 +31,19 @@ class Organization:
     date_include : datetime
     date_updated : datetime
 
+class ConverterOrganizationToJSON():
+    
+    @staticmethod
+    def __get_converted_org(organization) -> dict:
+        return asdict(organization)
+    
+    @classmethod
+    def get_converted_orgs(cls, organization) -> iter:
+        return (cls.__get_converted_org(org) for org in organization)
 class OrganizatonMaker:
     
     @staticmethod
-    def __get_id_and_url(record):
+    def __get_id_and_url(record) -> tuple:
         try:
             tag = record.find('div', class_='registry-entry__header-mid__number').find('a')
         except:
@@ -44,7 +54,7 @@ class OrganizatonMaker:
         return id, url
     
     @staticmethod
-    def __get_name(record):
+    def __get_name(record) -> str:
         try:
             return record.find('div', class_='registry-entry__body-value').text.strip()
         except:
@@ -52,14 +62,14 @@ class OrganizatonMaker:
         
     
     @staticmethod
-    def __get_inn(record):
+    def __get_inn(record) -> str:
         try:
             return record.find_all('div', class_='registry-entry__body-value')[1].text().strip()
         except:
             return None
 
     @staticmethod
-    def __get_date(record, num=0):
+    def __get_date(record, num=0) -> datetime:
         try:
             return datetime.strptime(record.find('div', class_='mt-auto').find_all('div', class_='data-block__value')[num].text, 
                                      r'%D.%M.%Y')
@@ -67,16 +77,16 @@ class OrganizatonMaker:
             return None 
 
     @classmethod
-    def __get_include_date(cls, record):
+    def __get_include_date(cls, record) -> datetime:
         return cls.__get_date(record)
     
 
     @classmethod
-    def __get_update_date(cls, record):
+    def __get_update_date(cls, record) -> datetime:
         return cls.__get_date(record, 1)
 
     @classmethod
-    def get_organization_from_record(cls, record):
+    def get_organization_from_record(cls, record) -> Organization:
         name = cls.__get_name(record)
         id, url = cls.__get_id_and_url(record)
         inn = cls.__get_inn(record)
@@ -85,82 +95,68 @@ class OrganizatonMaker:
         return Organization(id, name, inn, url, date_include, date_update)
     
 
-
 class ZakupkiParser(Parser):
-
-    url = Template(r'https://zakupki.gov.ru/epz/dishonestsupplier/search/results.html?searchString=&morphology=on&search-filter=Дате+обновления&savedSearchSettingsIdHidden=&sortBy=UPDATE_DATE&pageNumber=$page&sortDirection=false&recordsPerPage=_500&showLotsInfoHidden=false&fz223=on&ppRf615=on&dsStatuses=&inclusionDateFrom=&inclusionDateTo=&lastUpdateDateFrom=&lastUpdateDateTo=')
+    recs_per_page = 500
+    url = Template(f'https://zakupki.gov.ru/epz/dishonestsupplier/search/results.html?searchString=&morphology=on&search-filter=Дате+обновления&savedSearchSettingsIdHidden=&sortBy=UPDATE_DATE&pageNumber=$page&sortDirection=false&recordsPerPage=_{recs_per_page}&showLotsInfoHidden=false&fz223=on&ppRf615=on&dsStatuses=&inclusionDateFrom=&inclusionDateTo=&lastUpdateDateFrom=&lastUpdateDateTo=')
     
     @classmethod
-    def get_url_by_page_num(cls, page_num):
+    def get_url_by_page_num(cls, page_num) -> str:
         return cls.url.substitute(page=page_num)
 
     @classmethod
-    def get_page_by_page_num(cls, num=1):
+    def get_page_by_page_num(cls, num=1) -> str:
         url = cls.get_url_by_page_num(num)
         return get(url, headers=headers).text
 
     @classmethod
-    async def async_get_page_by_page_num(cls, loop, num=1):
-        print(f'Page: {num}')
+    async def async_get_page_by_page_num(cls, num=1) -> asyncio.coroutine:
         url = cls.get_url_by_page_num(num)
-        page = loop.run_in_executor(None, get, url)
-        return await page
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                page = await response.text()
+                print("Parsed: ", num)
+
+        return page
 
     @classmethod
-    async def get_pages(cls, loop, page_count):
-        return (cls.async_get_page_by_page_num(loop, page_num) for page_num in range(1, page_count+1))    
-
+    async def get_pages(cls, page_count) -> asyncio.coroutine:
+        pages = asyncio.gather(*(cls.async_get_page_by_page_num(page_num) 
+                    for page_num in range(1, page_count+1)))
+        return await pages
+    
     @staticmethod
-    def get_soup_from_html_page(html_page):
+    def get_soup_from_html_page(html_page) -> BeautifulSoup:
         # print(html_page)
         return BeautifulSoup(html_page, 'lxml')
 
     @classmethod
-    def __get_count_pages(cls):
+    def __get_count_pages(cls) -> int:
         page = cls.get_soup_from_html_page(cls.get_page_by_page_num())
         return int(page.find_all('li', class_='page')[-1].text.strip())
     
     @classmethod
-    def pasre_page(cls, page, org_maker):
+    def pasre_page(cls, page, org_maker) -> [Organization]:
         records = cls.get_soup_from_html_page(page).find('div', class_='search-registry-entrys-block')\
                                              .find_all('div', class_='search-registry-entry-block')
         
-        organizations = [org_maker.get_organization_from_record(record) for record in records]        
+        organizations = [org_maker.get_organization_from_record(record) for record in records[:10]]
         return organizations
 
-
     @classmethod
-    def parse(cls, org_maker):
+    def parse(cls, org_maker) -> [Organization]:
         page_count = cls.__get_count_pages()
-        print(page_count)
-        loop = asyncio.get_event_loop()
-        pages = cls.get_pages(loop, page_count)
-        print(type(pages))
-        pages = loop.run_until_complete(pages)
-        print(next(iter))
-        orgranizations = [cls.pasre_page(page, org_maker) for page in pages]
-        
+
+        pages = cls.get_pages(page_count)
+        pages = asyncio.run(pages)
+
+        print('SCRAPING')
+        orgranizations = reduce(lambda a, b: a + b, [cls.pasre_page(page, org_maker) for page in pages])
         return orgranizations
         
 
 if __name__ == "__main__":
     org_maker = OrganizatonMaker()
     orgs = ZakupkiParser().parse(org_maker)
-    # print(orgs)
-# async def foo():
-#     print('Running in foo')
-#     await asyncio.sleep(10)
-#     print('Explicit context switch to foo again')
-
-
-# async def bar():
-#     print('Explicit context to bar')
-#     await asyncio.sleep(10)
-#     print('Implicit context switch back to bar')
-
-
-# ioloop = asyncio.get_event_loop()
-# tasks = [ioloop.create_task(foo()), ioloop.create_task(bar())]
-# wait_tasks = asyncio.wait(tasks)
-# ioloop.run_until_complete(wait_tasks)
-# ioloop.close()
+    print(len(orgs))
+    conv = ConverterOrganizationToJSON()
+    pprint(conv.get_converted_orgs(orgs))
