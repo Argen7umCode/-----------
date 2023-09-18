b1eb0c67e19f28cc2e9b3580ead952a86bff341b
argen7um <argen7um.music@yandex.ru>
2023-09-18 09:34:23 +0700

    [ADD] refact. add abc Converter, add cls CSV Conv

--- parsers.py
+++ parsers.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
 from string import Template
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, astuple
 from datetime import datetime
 from pprint import pprint
 from functools import reduce
 from json import dump
+from typing import Any, Union
+import csv
+
 
 from requests import get, post
 from bs4 import BeautifulSoup
@@ -15,19 +18,24 @@ import aiohttp
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
     }
+class Saver:
+    @staticmethod
+    def save(data, path):
+        pass
 
-class JSONSaver:
+class JSONSaver(Saver):
     
     @staticmethod
     def save(data, path) -> None:
         with open(path, 'w', encoding='utf-8') as file:
             dump(data, file, indent=4, ensure_ascii=False)
 
-class Parser:
-
-    @abstractmethod
-    def parse(self):
-        ...
+class CSVSaver(Saver):
+    
+    @staticmethod
+    def save(data, path) -> None:
+        with open(path, 'w', newline='') as file:
+            csv.writer(file).writerows(data)
 
 @dataclass
 class Organization:
@@ -39,26 +47,22 @@ class Organization:
     date_include : datetime
     date_updated : datetime
     
-class ConverterOrganizationToJSON():
+class Maker(ABC):
     
-    @staticmethod
-    def __get_converted_org(organization) -> dict:
-        return asdict(organization)
-    
-    @classmethod
-    def get_converted_orgs(cls, organization) -> [dict]:
-        return [cls.__get_converted_org(org) for org in organization]
-    
+    @abstractmethod
+    def make(data):
+        pass
+
 class OrganizatonMaker:
     
     @staticmethod
     def __get_id_and_url(record) -> tuple:
         try:
             tag = record.find('div', class_='registry-entry__header-mid__number').find('a')
-        except:
+        except Exception:
             url, id = None, None
         else:
-            url = tag.get('href').strip() 
+            url = tag.get('href').strip()
             id = tag.text.strip().split()[-1]
         return id, url
     
@@ -66,42 +70,88 @@ class OrganizatonMaker:
     def __get_name(record) -> str:
         try:
             return record.find('div', class_='registry-entry__body-value').text.strip()
-        except:
+        except Exception:
             return None
         
-    
     @staticmethod
     def __get_inn(record) -> str:
         try:
             return record.find_all('div', class_='registry-entry__body-value')[1].text.strip()
-        except:
+        except Exception:
             return None
 
     @staticmethod
     def __get_date(record, num=0) -> str:
         try:
             return record.find('div', class_='mt-auto')\
-                         .find_all('div', class_='data-block__value')[num].text.strip() 
-        except:
-            return None 
+                         .find_all('div', class_='data-block__value')[num].text.strip()
+        except Exception:
+            return None
 
     @classmethod
     def __get_include_date(cls, record) -> str:
         return cls.__get_date(record)
     
-
     @classmethod
     def __get_update_date(cls, record) -> str:
         return cls.__get_date(record, 1)
 
     @classmethod
-    def get_organization_from_record(cls, record) -> Organization:
+    def make(cls, record) -> Organization:
         name = cls.__get_name(record)
         id, url = cls.__get_id_and_url(record)
         inn = cls.__get_inn(record)
         date_include = cls.__get_include_date(record)
         date_update = cls.__get_update_date(record)
         return Organization(id, name, inn, url, date_include, date_update)
+            
+
+class Converter(ABC):
+
+    @abstractmethod
+    def _convert_one(data: Any):
+        pass            
+
+    @abstractmethod
+    def _convert_many(data: Any):
+        pass
+
+    @abstractmethod
+    def convert(cls, data: Any, *args, **kwargs):
+        return cls._convert_one(data) if isinstance(data, Organization)\
+            else cls._convert_many(data)
+
+class ConverterOrganizationToJSON(Converter):
+    
+    @staticmethod
+    def _convert_one(organization: Organization) -> dict:
+        return asdict(organization)
+    
+    @classmethod
+    def _convert_many(cls, organizations: [Organization]) -> [dict]:
+        return [cls._convert_one(org) for org in organizations]
+
+
+class ConverterOrganizatonToCVS(Converter):
+    
+    @staticmethod
+    def _convert_one(organization: Organization, sep : str = ',') -> str:
+        return sep.join(astuple(organization))     
+
+    @classmethod
+    def _convert_many(cls, organizations: [Organization], sep : str = ',') -> str:
+        return '/n'.join(cls._convert_one(organization, sep) 
+                          for organization in organizations)
+    @classmethod
+    def convert(cls, data: Any, sep : str = ',', *args, **kwargs):
+        return cls._convert_one(data, sep) if isinstance(data, Organization)\
+            else cls._convert_many(data, sep)
+
+class Parser:
+
+    @abstractmethod
+    def parse(self):
+        pass
 
 class ZakupkiParser(Parser):
     recs_per_page = 500
@@ -147,13 +197,12 @@ class ZakupkiParser(Parser):
         return cls.get_soup_from_html_page(page).find('div', class_='search-registry-entrys-block')\
                                              .find_all('div', class_='search-registry-entry-block')
         
-
     @classmethod
     def pasre_page(cls, page, org_maker) -> [Organization]:
         records = cls.get_records_from_page(page)
-        organizations = [org_maker.get_organization_from_record(record) 
-                         for record in records]
-        return organizations
+        return [
+            org_maker.make(record) for record in records
+        ]
 
     @classmethod
     def parse(cls, org_maker) -> [Organization]:
@@ -163,17 +212,10 @@ class ZakupkiParser(Parser):
         pages = asyncio.run(pages)
 
         print('SCRAPING')
-        orgranizations = reduce(lambda a, b: a + b, [cls.pasre_page(page, org_maker) 
-                                                     for page in pages])
-        return orgranizations
+        return reduce(
+            lambda a, b: a + b, [cls.pasre_page(page, org_maker) for page in pages]
+        )
         
 
 if __name__ == "__main__":
-    org_maker = OrganizatonMaker()
-    parser = ZakupkiParser()
-    json_saver = JSONSaver()
-    conv = ConverterOrganizationToJSON()
-
-    orgs = parser.parse(org_maker)
-    data = conv.get_converted_orgs(orgs)
-    json_saver.save(data, 'test.json')
+    pprint(dir(ConverterOrganizatonToCVS))
